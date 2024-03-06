3d84f9c517a2f166f9c834e25ad2e50f701d2bcb
Leonardo <125489362+Z0rakiN@users.noreply.github.com>
2024-03-06 20:38:42 -0300

    Aula 06-03-2024

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import logo from './assets/002-1-react-native.png'
 import Props from './src/ex_2';
 import Menssagem from './src/components/menssagem';
+import Exemplo3 from './src/ex_3';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Menssagem value={'Exemplo 2'} />
+      {/* <Menssagem value={'Exemplo 2'} /> */}
+      <Exemplo3 />
     </View>
   );
 }
---
+++ src/ex_3/index.js
@@ -0,0 +1,11 @@
+import { View, Text, TouchbleOpacity} from 'react-native'
+
+import styles from './styles'
+
+export default function Exemplo3(){
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo 3</Text>
+        </View>
+    )
+}
